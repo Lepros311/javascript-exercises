f5ea063e42e1538004d31ffe89214bb1c58c7bec
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-02-26 18:31:29 -0500

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.split('').filter(function(char) {
+        return /[A-Za-z0-9]/.test(char);
+    });
+    str = str.join('').toLowerCase();
+    reversed_str = str.split('').reverse().join('');
+    return reversed_str === str;
 };
 
 // Do not edit below this line
