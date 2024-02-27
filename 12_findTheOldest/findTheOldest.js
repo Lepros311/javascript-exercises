5995b3a551daecb927f1b7b801658a95be4f54c7
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-02-26 19:13:44 -0500

    Attempt 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let peopleWithAge = people.map((person) => {
+        person.map((details) => {
+            details[age] = details.yearOfDeath - details.yearOfBirth;
+        })
+    })
+    return peopleWithAge;
 };
 
 // Do not edit below this line
